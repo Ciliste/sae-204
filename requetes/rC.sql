1aea9221faffad074029537962421a61c4d479fd
Théo Airey <theo.airey@gmail.com>
2022-06-01 15:08:15 +0200

    Réparation des conneries

---
+++ requetes/rC.sql
@@ -0,0 +1,24 @@
+/* c) Lister les clients (numéro, nom) avec leur coût total d’achats (que l’on nommera
+coutA) triés par leur coût décroissant, qui ont totalisé au moins 50000€
+d’achats... */
+
+SELECT    v.numClient, nomClient, SUM(cnc.prixVente * cnc.quantite) AS couta
+FROM      Vente v NATURAL JOIN Client cli NATURAL JOIN Concerner cnc
+GROUP BY  v.numClient, nomClient
+HAVING    SUM(cnc.prixVente * cnc.quantite) > 50000
+ORDER BY  couta DESC;
+
+/* 
+Résultat :
+
+ numclient |  nomclient  |   couta   
+-----------+-------------+-----------
+         4 | Poret       | 129209.55
+         6 | Timable     |   93406.5
+         8 | Ohm         |   92865.9
+         7 | Don Devello |   82909.4
+         9 | Ginal       |   66003.9
+        10 | Hautine     |   59519.1
+         5 | Menvussa    |   56772.3
+         3 | Hauraque    |     51684
+*/
