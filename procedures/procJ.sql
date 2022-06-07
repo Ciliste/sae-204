109b67729b5b7d39c2d276541f2733207264d678
Théo Airey <theo.airey@gmail.com>
2022-06-07 14:57:07 +0200

    Procédure J

--- procedures/procJ.sql
+++ procedures/procJ.sql
@@ -9,6 +9,51 @@
 --son classement d'acheteur décrit ci-dessus.
 --NB : on pourra avantageusement utiliser une boucle FOR ou un curseur... 
 
-CREATE TYPE typeProcJ
+DROP TYPE IF EXISTS rangClient CASCADE;
+CREATE TYPE rangClient AS (
+    idClient NUMERIC,
+    nomClient VARCHAR,
+    rang NUMERIC
+);
 
-CREATE OR REPLACE FUNCTION 
+DROP FUNCTION IF EXISTS procJBis CASCADE;
+CREATE OR REPLACE FUNCTION procJBis() 
+RETURNS SETOF rangClient AS
+$$
+DECLARE
+    retType rangClient;
+    ordre RECORD;
+    cptRang INTEGER := 1;
+BEGIN
+    FOR ordre IN SELECT Client.numClient, SUM(Concerner.quantite) AS somme FROM Client NATURAL JOIN Vente NATURAL JOIN Concerner GROUP BY Client.numClient ORDER BY somme DESC
+    LOOP
+        SELECT ordre.numClient  INTO retType.idClient;
+        SELECT Client.nomClient INTO retType.nomClient FROM Client WHERE Client.numClient = ordre.numClient;
+        SELECT cptRang INTO retType.rang;
+        cptRang = cptRang + 1;
+        RETURN NEXT retType;
+    END LOOP;
+END
+$$
+LANGUAGE Plpgsql;
+
+SELECT * FROM procJBis();
+
+/*
+\i procJ.sql
+
+ idclient |  nomclient  | rang 
+----------+-------------+------
+        4 | Poret       |    1
+        6 | Timable     |    2
+        8 | Ohm         |    3
+        7 | Don Devello |    4
+        9 | Ginal       |    5
+        3 | Hauraque    |    6
+        5 | Menvussa    |    7
+       10 | Hautine     |    8
+        2 | Fissile     |    9
+        1 | Torguesse   |   10
+       11 | Kament      |   11
+(11 lignes)
+*/
