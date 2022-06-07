4810e7da88782ead1e0cf8dbed6f769186746d41
Théo Airey <theo.airey@gmail.com>
2022-06-07 13:21:34 +0200

    Procédure I

--- procedures/procI.sql
+++ procedures/procI.sql
@@ -1,17 +1,40 @@
---Ecrire une fonction qui supprime une vente dont l'identifiant est passé enparamètre.
---Vérifier d'abord que la vente associée à l'identifiant existe, si elle n'existe pas
---afficher un message d'erreur le mentionnant; si elle existe on la supprime.
---Cette suppression va générer une violation de clé étrangère dans la table
---‘Concerner’.
---Pour gérer cela, on utilisera le code d'erreur FOREIGN_KEY_VIOLATION
---dans un bloc EXCEPTION dans lequel on supprimera tous les tuples de la table
---‘Concerner’ qui possèdent ce numéro de vente, avant de supprimer la venet ellemême. On pourra au passage afficher aussi un message d'avertissement sur cette
---exception
+-- Ecrire une fonction qui supprime une vente dont l'identifiant est passé en
+-- paramètre.
+-- Vérifier d'abord que la vente associée à l'identifiant existe, si elle n'existe pas
+-- afficher un message d'erreur le mentionnant; si elle existe on la supprime.
+-- Cette suppression va générer une violation de clé étrangère dans la table
+-- ‘Concerner’.
+-- Pour gérer cela, on utilisera le code d'erreur FOREIGN_KEY_VIOLATION
+-- dans un bloc EXCEPTION dans lequel on supprimera tous les tuples de la table
+-- ‘Concerner’ qui possèdent ce numéro de vente, avant de supprimer la venet ellemême. On pourra au passage afficher aussi un message d'avertissement sur cette
+-- exception.
 
-CREATE OR REPLACE FUNCTION proc_i ( idVente Vente.numVente%TYPE )
-    RETURNS void
-    AS $$
-
+DROP FUNCTION IF EXISTS procI CASCADE;
+CREATE OR REPLACE FUNCTION procI(idVente Vente.numVente%TYPE)
+RETURNS VOID AS
+$$
 BEGIN
+    PERFORM * FROM Vente WHERE Vente.numVente = idVente;
+    IF (NOT FOUND) THEN
+        RAISE EXCEPTION 'La vente n existe pas...';
+    END IF;
+    DELETE FROM Vente WHERE Vente.numVente = idVente;
+EXCEPTION
+    WHEN FOREIGN_KEY_VIOLATION
+    THEN
+        DELETE FROM Concerner WHERE Concerner.numVente = idVente;
+        DELETE FROM Vente     WHERE Vente.numVente     = idVente;
+END
+$$
+LANGUAGE Plpgsql;
 
-    
+SELECT procI(1);
+
+/*
+\i procI.sql
+
+ proci 
+-------
+ 
+(1 ligne)
+*/
